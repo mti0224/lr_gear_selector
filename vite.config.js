fa3db6e4fd01a8cdbfdd07dfbb6912c4e004b8dc
mit0224 <38598765+mti0224@users.noreply.github.com>
2025-11-02 02:22:05 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 export default defineConfig({
+  // 一定要對應你的 repo 名稱
+  base: '/lr_gear_selector/',
   plugins: [react()],
-  base: '/', // 本機無所謂，用 '/' 最直覺
 })
